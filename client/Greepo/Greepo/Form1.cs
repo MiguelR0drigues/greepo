2eaa6f0d3bfd4900802049d78d2227a5b0ae9327
telmoss7 <telmo_escapaes@hotmail.com>
2022-12-13 23:27:28 +0000

    Mapa de Portugal
    falta zoom, dar nome aos distritos

---
+++ client/Greepo/Greepo/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greepo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LiveCharts.WinForms.GeoMap geoMap = new LiveCharts.WinForms.GeoMap();
+            geoMap.Source = $"{Application.StartupPath}/World.xml";
+            this.Controls.Add(geoMap);
+            geoMap.Dock= DockStyle.Fill;    
+        }
+    }
+}
